dedc6e5e0f45e9578bcc44ba6ee07ff5820ca8f7
tommywalkie <tommywalkie@gmail.com>
2020-10-09 12:18:27 +0200

    Added documentation for abstract trait methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,45 @@
-#[macro_use]
-extern crate doc_comment;
+use doc_comment::doc_comment;
 use duplicate::{duplicate_inline};
 
 /// Trait denoting a vector `Vec<T>` which can be turned into `Vec<U>`
 pub trait TurnableVec {
     duplicate_inline!{
         [
-            to_some_format  return_type;
-            [ to_veci8 ]    [ i8 ];
-            [ to_veci16 ]    [ i16 ];
-            [ to_veci32 ]    [ i32 ];
-            [ to_veci64 ]    [ i64 ];
-            [ to_veci128 ]    [ i128 ];
-            [ to_vecu8 ]    [ u8 ];
-            [ to_vecu16 ]    [ u16 ];
-            [ to_vecu32 ]    [ u32 ];
-            [ to_vecu64 ]    [ u64 ];
-            [ to_vecu128 ]    [ u128 ];
-            [ to_vecf32 ]    [ f32 ];
-            [ to_vecf64 ]    [ f64 ];
+            doc to_some_format  return_type;
+            ["Returns a vector with `i8` values"] [ to_veci8 ]    [ i8 ];
+            ["Returns a vector with `i16` values"] [ to_veci16 ]    [ i16 ];
+            ["Returns a vector with `i32` values"] [ to_veci32 ]    [ i32 ];
+            ["Returns a vector with `i64` values"] [ to_veci64 ]    [ i64 ];
+            ["Returns a vector with `i128` values"] [ to_veci128 ]    [ i128 ];
+            ["Returns a vector with `u8` values"] [ to_vecu8 ]    [ u8 ];
+            ["Returns a vector with `u16` values"] [ to_vecu16 ]    [ u16 ];
+            ["Returns a vector with `u32` values"] [ to_vecu32 ]    [ u32 ];
+            ["Returns a vector with `u64` values"] [ to_vecu64 ]    [ u64 ];
+            ["Returns a vector with `u128` values"] [ to_vecu128 ]    [ u128 ];
+            ["Returns a vector with `f32` values"] [ to_vecf32 ]    [ f32 ];
+            ["Returns a vector with `f64` values"] [ to_vecf64 ]    [ f64 ];
         ]
-        fn to_some_format(&self) -> Vec<return_type>;
+        doc_comment!(doc, fn to_some_format(&self) -> Vec<return_type>;);
     }
 }
 
 duplicate_inline!{
     [
-        original_type;
-        [ i8 ];
-        [ i16 ];
-        [ i32 ];
-        [ i64 ];
-        [ i128 ];
-        [ u8 ];
-        [ u16 ];
-        [ u32 ];
-        [ u64 ];
-        [ u128 ];
-        [ f32 ];
-        [ f64 ];
+        docu original_type;
+        ["Apply `TurnableVec` trait to a `Vec<i8>` vector"] [ i8 ];
+        ["Apply `TurnableVec` trait to a `Vec<i16>` vector"] [ i16 ];
+        ["Apply `TurnableVec` trait to a `Vec<i32>` vector"] [ i32 ];
+        ["Apply `TurnableVec` trait to a `Vec<i64>` vector"] [ i64 ];
+        ["Apply `TurnableVec` trait to a `Vec<i128>` vector"] [ i128 ];
+        ["Apply `TurnableVec` trait to a `Vec<u8>` vector"] [ u8 ];
+        ["Apply `TurnableVec` trait to a `Vec<u16>` vector"] [ u16 ];
+        ["Apply `TurnableVec` trait to a `Vec<u32>` vector"] [ u32 ];
+        ["Apply `TurnableVec` trait to a `Vec<u64>` vector"] [ u64 ];
+        ["Apply `TurnableVec` trait to a `Vec<u128>` vector"] [ u128 ];
+        ["Apply `TurnableVec` trait to a `Vec<f32>` vector"] [ f32 ];
+        ["Apply `TurnableVec` trait to a `Vec<f64>` vector"] [ f64 ];
     ]
-    /// Apply `TurnableVec` trait to some numeric format
-    impl TurnableVec for Vec<original_type> {
+    doc_comment!(docu, impl TurnableVec for Vec<original_type> {
         duplicate_inline!{
             [
                 doc to_some_format  return_type;
@@ -66,7 +64,7 @@ duplicate_inline!{
                 res
             });
         }
-    }
+    });
 }
 
 #[cfg(test)]
